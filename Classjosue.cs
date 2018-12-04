8bdc394fc9ef02d9ac27d3908a5ab6794138b52f
donatnshimiyimana <donatnshimiyimana@gmail.com>
2018-12-04 15:24:56 +0200

    added csharp program

---
+++ Classjosue.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    class Classjosue
+    {
+         public double num1;
+        public double num2;
+        public double addition(double num1, double num2){
+            return (num1+num2);
+        }
+        public double subtruction(double num1, double num2){
+            return (num1-num2);
+        }
+        public double multiplication(double num1, double num2){
+            return (num1*num2);
+        }
+        public double division(double num1, double num2){
+            if (num2==0)
+            Console.WriteLine("invalid dnominator");
+            return (num1/num2); 
+        }
+
+        public void operators()
+        {
+            Console.WriteLine("1.addition");
+            Console.WriteLine("2.subtruction");
+            Console.WriteLine("3.multiplication");
+            Console.WriteLine("4.division");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double number1;
+            double number2;
+            int choose;
+
+            Console.WriteLine("enter num1");
+            number1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("enter number2");
+            number2 = Convert.ToDouble(Console.ReadLine());
+
+            var abj = new Classjosue();
+            abj.operators();
+
+            Console.WriteLine("choose an operation");
+            choose = Convert.ToInt32 (Console.ReadLine());
+
+            switch(choose)
+            {
+                case 1:
+                    Console.WriteLine("addition= {0}", abj.addition(number1, number2));
+                    break;
+                case 2:
+                    Console.WriteLine("subtruction= {0}", abj.subtruction(number1, number2));
+                    break;
+                case 3:
+                    Console.WriteLine("multiplication= {0}", abj.multiplication(number1, number2));
+                    break;
+                case 4:
+                    Console.WriteLine("division= {0}", abj.division(number1, number2));
+                    break;
+            }
+           
+
+            Console.ReadLine();
+        }
+    }
+}
